6425d1769bad563f7113bf184516e54a7a22c2dc
wkrwez <2840853679@qq.com>
2024-04-28 21:08:51 +0800

    轮播图

--- vue/Swiper/Swip/src/views/Swiper.vue
+++ vue/Swiper/Swip/src/views/Swiper.vue
@@ -1,84 +1,129 @@
 <template>
     <div>
-        <div  class="all">
-           <div class="picture">
-            <div  class="all-img" v-for="item in props.list" :key="item.id"> 
-                <img :src=" item.imgUrl ">
+        <div class="all">
+            <div class="picture" :class="{'transfor':count !==0}">
+                <div class="all-img "  v-for="item in props.list" :key="item.id">
+                    <img :src="item.imgUrl">
+                </div>
             </div>
-        </div>
             <div class="left active" @click="getLeft">+</div>
             <div class="right active" @click="getRight">-</div>
-            <ul class="all-item" @click="getBlack">
-                <li v-for="(item,index) in list.length" :key="index"></li>
-            </ul>
-            
+            <div class="all-item">
+                <span @click="getBlack" v-for="(item, index) in props.list" :key="index"></span>
+            </div>
+
         </div>
     </div>
 </template>
 
 <script setup>
-import Swipers from '../Swiper.js'
-import {onMounted, ref} from 'vue'
 
+import { onMounted, ref } from 'vue'
 
+onMounted(() => {
+    continueRun()
+})
 
 const current = ref(0)
-const content = ref(0)
+const content = ref(0)  //指哪张图片
+const count = ref(0)  //轮播计数
+const timer = ref(null) //启动轮播的定时器
+const tims = ref(null) //点击小黑点启动轮播
 
 
 const props = defineProps({
-    list:Array
+    list: Array
 })
 
 
-const getLeft = (id)=>{
+const getLeft = (id) => {
 
 }
 
-const getRight = ()=>{
+const getRight = () => {
+    count.value++
+    if (count.value > 4) {
+        count.value = 0
+    }
+
+    content.value = count.value * -800 + 'px'
+    clearTimeout(timer.value)
+    tims.value = null
+
+    tims.value = continueRun()
 
 }
 
-const getBlack= (e)=>{
-   current.value = e.target.__vnode.key
-   content.value = current.value*-800 + 'px'
+//几个小黑点
+const getBlack = (e) => {
+    current.value = e.target.__vnode.key
+    content.value = current.value * -800 + 'px'
+    clearTimeout(timer.value)
+    clearTimeout(tims.value)
+
+    tims.value = setTimeout(() => {
+        continueRun()
+    }, 1000)
+
 
 }
+//启动轮播
+const Run = () => {
+    return new Promise((resolve) => {
+        timer.value = setTimeout(() => {
+            count.value = count.value + 1
+            if (count.value > 4) {
+                count.value = 0
+            }
+            content.value = count.value * -800 + 'px'
+            resolve()
+        }, 2000);
+    })
 
+}
+//持续启动轮播
+const continueRun = () => {
+    Promise.resolve().then(() => {
+        return Run()
+    }).then(() => {
+        return continueRun()
+    })
+}
 
 </script>
 
 <style lang="less" scoped>
-.all{
+.all {
     height: 400px;
-    width:800px;
+    width: 800px;
     background-color: #eb7676;
     display: flex;
     overflow: hidden;
     position: relative;
     margin: 100px auto;
-    .picture{
+    
+    .picture {
         position: absolute;
         display: flex;
-        left:v-bind(content);
-        .all-img{
-        display: flex;
-        left: 0;
+        left: v-bind(content);
         
-        img{
-           
-           width: 800px;
-           height: 400px;
-           object-fit: cover; 
-        //    
-           
+        img {
+            width: 800px;
+            height: 400px;
+            object-fit: cover;
+            
         }
         
+
+        
     }
+    //图片的平滑切换
+    .transfor{
+        transition: left 0.5s ease;
     }
 
-    
-    .active{
+    //按钮的公共样式
+    .active {
         background-color: #372b2b;
         height: 400px;
         width: 50px;
@@ -89,28 +134,39 @@ const getBlack= (e)=>{
         align-items: center;
         justify-content: center;
     }
-    .left{
-        left: 0;  
+
+    .left {
+        left: 0;
     }
-    .right{
+
+    .right {
         right: 0;
     }
-    .all-item{
+
+    .all-item {
         position: absolute;
-        width: 800px;
-        height: 50px;
-        color: #000;
+        width: 100%;
+        height: 20px;
         display: flex;
         bottom: 0;
         justify-content: center;
         align-items: center;
-        li{
+
+        span {
             width: 20px;
             height: 20px;
-            font-size: 30px;
-            
+
+            background-color: black;
+            margin: 5px;
+            border-radius: 50%;
+
+            .li-active {
+                color: white
+            }
+
         }
     }
 
 }
+
 </style>
