6f4bec324deec45082b7336d2f82c750b5d04b76
wkrwez <2840853679@qq.com>
2026-05-28 09:34:48 +0800

    Feat:vue2冻结代理测试

---
+++ vue/vue2/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <button @click="change">change</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      data: Object.freeze({
+        name: "1",
+        id: 1,
+        age: 1,
+        children: {
+          name: "2",
+          id: 2,
+          age: 2,
+          children: [
+            {
+              name: "3",
+              id: 3,
+              age: 3,
+            },
+          ],
+        },
+      }),
+    };
+  },
+  methods: {
+    change() {
+      console.log(this.data);
+    },
+  },
+};
+</script>
+
+<style></style>
